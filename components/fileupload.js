d59f917ffa456e53b3f559b9cbbf3d045c38da2a
SeptAlfauzan <alfauzansepta@gmail.com>
2022-01-18 04:43:48 +0700

    add responsize to canvas

--- components/fileupload.js
+++ components/fileupload.js
@@ -1,5 +1,5 @@
 import { Router, useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import { getExtension, getFileFakePath } from "../lib/handleFileUpload";
 import Detail from "./detail";
@@ -14,7 +14,10 @@ function ContainerDragDrop() {
     )
 }
 
-function DragDropFile(params) {
+const DragDropFile = ({isHome, handleToPreview}) => {
+    useEffect(()=>{
+        isHome && setEnter(false)
+    },[isHome])
     const router = useRouter();
     const [file, setFile] = useState(null);
     const [fileExtensions, setFileExtensions] = useState(null);
@@ -24,14 +27,14 @@ function DragDropFile(params) {
 
     const handleChange = (file) => {
         const { fileData, fileFakePath } = getFileFakePath(file);
-        // console.log('uploaded', fileFakePath);
+        
         if (fileFakePath == '') return setEnter(false);// handle when user cancel uploading file
         const extensions = getExtension(fileFakePath);
-        // console.log(extension);
-        // console.log(URL.createObjectURL(fileData));
-        setFile(URL.createObjectURL(fileData));
+        const fileURL = URL.createObjectURL(fileData);
+        setFile(fileURL);
         setFileExtensions(extensions);
-        setUploaded(true);
+        
+        handleToPreview();
     };
 
     const handleEnterDrag = () => {
@@ -44,18 +47,17 @@ function DragDropFile(params) {
 
     return (
         <div className="flex justify-center items-center full-width h-full">
-            {!uploaded && (
+            {isHome && (
                 <div className="w-3/4 flex-row">
                     <h3 className=' text-3xl mb-3  text-center'>Welcome To zyx3d.xyz</h3>
                     <p className='mb-10 text-gray-500  text-center'>Currently Support: .gltf, .obj, .fbx</p>
-
                     <div id='dropfile-container' className={`${enter ? 'border-black bg-gray-400' : 'border-gray-300'} transition-all duration-100 w-full rounded-2xl border-2 h-80 flex items-center justify-center relative`}>
                         <p>Drop your file here</p>
                         <input type="file" onDragEnter={handleEnterDrag} onDragLeave={handleLeaveDrag} onChange={(file) => handleChange(file)} className="h-80 bg-gray-100 opacity-0 absolute w-full"></input>
                     </div>
                 </div>
             )}
-            {uploaded && <Detail file={file} extensions={fileExtensions} />}
+            {!isHome && <Detail file={file} extensions={fileExtensions} />}
         </div>
     );
 }
--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Link from 'next/link';
 
-const Navbar = () => {
+
+const Navbar = ({handleToHome}) => {
+
+    const clickHandler = () => handleToHome();
     return(
         <nav className="w-full bg-gray-50 z-10 borders">
           <ul className='flex justify-end gap-3 p-5'>
             <li className='mr-auto'>
-              <a className='hover:to-blue-600'>
+              <a className='hover:to-blue-600 hover:cursor-pointer' onClick={clickHandler}>
                 Home
               </a>
             </li>
